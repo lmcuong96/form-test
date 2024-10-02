ddf1eef30b6b3e08f0517286078ddf2f71f7b46d
lmcuong96 <lmchaha96@gmail.com>
2024-10-02 12:09:46 +0700

    add: show username and password length

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
-
-function App() {
+import React from "react";
+export function App() {
+  const [state, setState] = React.useState({
+    username: "",
+    password: "",
+  });
   const handleSubmit = (event) => {
     event.preventDefault();
     const username = event.target.username.value;
     const password = event.target.password.value;
-
     if (!username || !password) {
       alert("Please enter username and password");
       return;
@@ -20,16 +23,38 @@ function App() {
       <form action="submit" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={(e) =>
+              setState({
+                ...state,
+                username: e.target.value,
+              })
+            }
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setState({
+                ...state,
+                password: e.target.value.length,
+              })
+            }
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
+      <div>
+        <p>Tên của bạn là {state.username}</p>{" "}
+        <p>Mật khẩu của bạn có {state.password} ký tự</p>
+      </div>
     </>
   );
 }
-
-export default App;
